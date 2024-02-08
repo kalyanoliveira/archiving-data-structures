4819d23d415df9159fd95195f3ca4b3321456130
kalyanoliveira <kalyan.c.o.010@gmail.com>
2024-02-08 11:59:05 -0500

    Documents enum inside a struct

--- week-1/structs.cpp
+++ week-1/structs.cpp
@@ -6,16 +6,26 @@
 
 int main(void)
 {
+    // We are going to be creating a `Person` struct, and people like days of
+    // the week. Hence, an enum!
+    enum Day {U, M, T, W, R, F, S} favorite_day_of_week;
+
     // You should really not be defining structs only inside main.
     // Unless you have a very good reason for it.
     // I have one: I'm lazy.
-    struct Person {std::string name; int age; bool alive;};
+    struct Person {
+        std::string name;
+        int age;
+        bool alive;
+        Day favorite_day_of_week;
+    };
 
-    Person someone = {"Daquavious", 1000, true};
+    Person someone = {"Daquavious", 1000, true, S};
 
-    std::cout << "Hi, my name is " + someone.name + ", I am " << 
-        someone.age << " years old, and I am " << 
-        (!someone.alive ? "not " : "") << "alive." << std::endl;
+    std::cout << "Hi, my name is " + someone.name + ", I am " << someone.age <<
+        " years old, I am " << (!someone.alive ? "not " : "") << "alive, "
+        << "and I love day " << someone.favorite_day_of_week << " of the week!"
+        << std::endl;
 
     return 0;
 }
