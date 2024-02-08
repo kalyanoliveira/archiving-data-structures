35773e09a2f2f7864166a4f2d6948465b5f9929d
kalyanoliveira <kalyan.c.o.010@gmail.com>
2024-02-08 14:07:27 -0500

    Adds cautionary tale about namespace conflicts

--- week-1/namespaces.cpp
+++ week-1/namespaces.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
+// You can define namespaces during compile time.
 namespace first {
     bool condition = true;
 }
 
+// Using statements are also done during compile time. There may be conflict
+// amongst namespaces!.
 using namespace first;
 
 int main(void)
 {
-
     if (condition) {
         std::cout << "The condition is true" << std::endl;
     } else {
