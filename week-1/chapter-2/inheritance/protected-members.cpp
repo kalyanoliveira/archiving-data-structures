c3dd3c0727f76d034972b1c0525a365272e684c5
kalyanoliveira <kalyan.c.o.010@gmail.com>
2024-02-11 12:55:02 -0500

    Documents protected members.

---
+++ week-1/chapter-2/inheritance/protected-members.cpp
@@ -0,0 +1,52 @@
+// In inheritance, a protected member of a base class is acessible by member
+// functions of children classes.
+
+#include <iostream>
+
+class Animal {
+    public:
+        Animal();
+
+    protected:
+        bool has_dna;
+};
+
+Animal::Animal() {
+    has_dna = true;
+}
+
+class Dog : public Animal {
+    public:
+        Dog(std::string b = "UNKNOWN");
+
+        std::string get_breed() const;
+
+        bool get_has_dna() const;
+
+    private:
+        std::string breed;
+};
+
+Dog::Dog(std::string b) {
+    breed = b;
+}
+
+std::string Dog::get_breed() const {
+    return breed;
+}
+
+// Since `has_dna` is a protected member of `Animal`, member functions of the
+// children class `Dog` should have access to `has_dna`.
+bool Dog::get_has_dna() const {
+    return has_dna;
+}
+
+int main(void) {
+
+    Dog d("wasssup"); 
+
+    // This should now work.
+    std::cout << d.get_has_dna() << std::endl;
+
+    return 0;
+}
