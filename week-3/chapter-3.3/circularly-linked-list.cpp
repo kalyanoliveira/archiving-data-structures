c841c579d45f6d259764011523c1a49a6347e0ba
kalyanoliveira <kalyan.c.o.010@gmail.com>
2024-02-14 16:52:52 -0500

    Finishes implementing circularly linked list

--- week-3/chapter-3.3/circularly-linked-list.cpp
+++ week-3/chapter-3.3/circularly-linked-list.cpp
@@ -16,7 +16,7 @@ class CircularlyLinkedNode {
 class CircularlyLinkedList {
     public:
         CircularlyLinkedList();
-        //~CircularlyLinkedList();
+        ~CircularlyLinkedList();
 
         bool empty() const;
 
@@ -25,12 +25,15 @@ class CircularlyLinkedList {
         void add_node_after_cursor(const int &v);
         void remove_node_after_cursor();
 
-        const CircularlyLinkedNode &get_back_node() const;
-        const CircularlyLinkedNode &get_front_node() const;
+        int get_back_node_value() const;
+        int get_front_node_value() const;
 
         void print();
 
     private:
+        // Notice that cursor simply points to a node.
+        // The node that is pointed by cursor, though, is **not** a sentinel
+        // node.
         CircularlyLinkedNode *cursor;
 };
 
@@ -38,11 +41,16 @@ CircularlyLinkedList::CircularlyLinkedList() {
     cursor = NULL;
 }
 
-// void CircularlyLinkedList::~CircularlyLinkedList() {
-// }
+CircularlyLinkedList::~CircularlyLinkedList() {
+    while(!empty()) { remove_node_after_cursor(); }
+}
 
 bool CircularlyLinkedList::empty() const {
     return cursor == NULL;
+}
+
+void CircularlyLinkedList::advance() {
+    cursor = cursor->next;
 }
 
 void CircularlyLinkedList::add_node_after_cursor(const int &v) {
@@ -72,6 +80,38 @@ void CircularlyLinkedList::add_node_after_cursor(const int &v) {
     cursor->next = new_node;
 }
 
+void CircularlyLinkedList::remove_node_after_cursor() {
+    // Create a pointer to the node that we have to delete.
+    CircularlyLinkedNode *node_to_delete = cursor->next;
+
+    // If we only have one node,
+    if (node_to_delete->next = node_to_delete) {
+        // Make the cursor NULL.
+        cursor = NULL;
+
+        // Now we can delete the node.
+        delete node_to_delete;
+
+        // And return.
+        return;
+    }
+
+    // Make the next of the node pointed by cursor be the next of the node to
+    // delete.
+    cursor->next = node_to_delete->next;
+
+    // Now we can delete the node.
+    delete node_to_delete;
+};
+
+int CircularlyLinkedList::get_back_node_value() const {
+    return cursor->value;
+}
+
+int CircularlyLinkedList::get_front_node_value() const {
+    return cursor->next->value;
+}
+
 void CircularlyLinkedList::print() {
     std::cout << "CircularlyLinkedList: ";
 
@@ -93,6 +133,13 @@ int main(void) {
     cll.add_node_after_cursor(7);
     cll.add_node_after_cursor(8);
     cll.add_node_after_cursor(9);
+
+    cll.print();
+
+    std::cout << cll.get_back_node_value() << std::endl;
+    std::cout << cll.get_front_node_value() << std::endl;
+    
+    cll.advance();
 
     cll.print();
 
