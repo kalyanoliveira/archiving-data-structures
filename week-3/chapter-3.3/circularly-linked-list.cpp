6b54ce0ec31c121fcb2db7921a035885b1e57c81
kalyanoliveira <kalyan.c.o.010@gmail.com>
2024-02-14 14:57:32 -0500

    Writes interface for a circularly linked list

---
+++ week-3/chapter-3.3/circularly-linked-list.cpp
@@ -0,0 +1,37 @@
+// In a circularly linked list, we have a cursor node that points to any random
+// node in the list.
+// The node pointed by the cursor is known as the back element.
+// The node after the node pointed by the cursor is known as the front element. 
+
+#include <iostream>
+
+class CircularlyLinkedNode {
+    friend class CircularlyLinkedList;
+
+    private:
+        int value;
+        CircularlyLinkedNode *next;
+};
+
+class CircularlyLinkedList {
+    public:
+        CircularlyLinkedList();
+        ~CircularlyLinkedList();
+
+        bool empty() const;
+
+        void advance();
+
+        void add_node_after_cursor(const int &v);
+        void remove_node_after_cursor();
+
+        const CircularlyLinkedNode &get_back_node() const;
+        const CircularlyLinkedNode &get_front_node() const;
+
+    private:
+        CircularlyLinkedNode *cursor;
+};
+
+int main(void) {
+    return 0;
+}
