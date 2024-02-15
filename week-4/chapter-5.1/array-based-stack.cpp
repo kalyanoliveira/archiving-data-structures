4548feb3230dec034ef983fecd708d913c984072
kalyanoliveira <kalyan.c.o.010@gmail.com>
2024-02-14 22:47:02 -0500

    Writes constructor and destructor of a stack.

--- week-4/chapter-5.1/array-based-stack.cpp
+++ week-4/chapter-5.1/array-based-stack.cpp
@@ -9,7 +9,9 @@
 
 class Stack {
     public:
-        Stack();
+        // We need to specify the capacity of a Stack when we construct it.
+        Stack(const int &c);
+
         // We a need destructor, since we are allocating space in memory.
         ~Stack();
 
@@ -25,6 +27,18 @@ class Stack {
         int number_of_elements;
 };
 
+Stack::Stack(const int &c) {
+    capacity = c; 
+    elements = new int[c];
+    number_of_elements = 0;
+}
+
+Stack::~Stack() {
+    delete [] elements;
+}
+
 int main(void) {
+    Stack s(10);
+
     return 0;
 }
