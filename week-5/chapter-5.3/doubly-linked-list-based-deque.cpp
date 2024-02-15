aa7dbb65432fb83fb9bff0f2b7a21eb91a726891
kalyanoliveira <kalyan.c.o.010@gmail.com>
2024-02-15 10:19:08 -0500

    Implements DLL for deque example.

--- week-5/chapter-5.3/doubly-linked-list-based-deque.cpp
+++ week-5/chapter-5.3/doubly-linked-list-based-deque.cpp
@@ -8,16 +8,25 @@ class Node {
     private:
         int value;
         Node *next;
+        Node *prev;
 };
 
 class DLL {
     public:
         DLL();
         
-        // // We must create a destructor because DLL allocates.
-        // ~DLL();
+        // We must create a destructor because DLL allocates.
+        ~DLL();
 
-        bool empty();
+        bool empty() const;
+
+        void add_node_after_head(const int &v);
+        void add_node_before_tail(const int &v);
+
+        void remove_node_after_head();
+        void remove_node_before_tail();
+
+        void print() const;
 
     private:
         // These are sentinel nodes.
@@ -33,7 +42,7 @@ DLL::DLL() {
 
     // Make the head and tail sentinel nodes point to each other.
     head->next = tail;
-    tail->preve = head;
+    tail->prev = head;
 
     // Make head and tail be the end of the linked list be ensuring NULL
     // pointers:
@@ -41,6 +50,101 @@ DLL::DLL() {
     tail->next = NULL;
 }
 
+DLL::~DLL() {
+    while (!empty()) remove_node_after_head();
+}
+
+bool DLL::empty() const {
+    return head->next == tail;
+}
+
+void DLL::add_node_after_head(const int &v) {
+    Node *new_node = new Node;
+    new_node->value = v;
+
+    // Make the next of the new node be the next of the node pointed by head.
+    new_node->next = head->next;
+
+    // Make the prev of the next of the node pointed by head by the new node.
+    // == this is the only place where all of this could not work lol ==
+    (head->next)->prev = new_node;
+
+    // Make the prev of the new_node be the node pointed by head.
+    new_node->prev = head; 
+
+    // Make the next of the node pointed by head be the new node.
+    head->next = new_node;
+}
+
+void DLL::add_node_before_tail(const int &v) {
+    Node *new_node = new Node;
+    new_node->value = v;
+
+    // Make the prev of the new node be the prev of the node pointed by tail;
+    new_node->prev = tail->prev;
+
+    // Make the next of the new node by the tail;
+    new_node->next = tail;
+
+    // Make the next of the prev of the node pointed by tail be the new node.
+    (tail->prev)->next = new_node;
+
+    // Make the prev of the tail be the new node;
+    tail->prev = new_node;
+}
+
+void DLL::remove_node_after_head() {
+    Node *node_to_remove = head->next;
+
+    // Make the next of the node pointed by head by the next of the
+    // node_to_delete.
+    head->next = node_to_remove->next;
+
+    // Make the prev of next of the node_to_remove be the head.
+    (node_to_remove->next)->prev = head;
+
+    delete node_to_remove;
+}
+
+void DLL::remove_node_before_tail() {
+    Node *node_to_remove = tail->prev;
+
+    // Make the prev of the node pointed by tail be the prev of the
+    // node_to_delete.
+    tail->prev = node_to_remove->prev;
+
+    // Make the next of the prev of the node_to_delete be the tail.
+    (node_to_remove->prev)->next = tail;
+
+    delete node_to_remove;
+}
+
+void DLL::print() const {
+    std::cout << "DLL: ";
+
+    for (Node *temp = head->next; temp->next != NULL; temp=temp->next) {
+        std::cout << temp->value << " ";
+    }
+
+    std::cout << std::endl;
+}
+
 int main(void) {
+    DLL dll;
+
+    dll.add_node_after_head(1);
+    dll.add_node_after_head(2);
+    dll.add_node_after_head(3);
+    dll.print();
+
+    dll.add_node_before_tail(1);
+    dll.add_node_before_tail(2);
+    dll.add_node_before_tail(3);
+    dll.print();
+
+    dll.remove_node_after_head();
+    dll.remove_node_before_tail();
+    dll.print();
+
     return 0;
 }
