77950b76f4d9ddb3db081cb29f7f87b95eadbc34
kalyanoliveira <kalyan.c.o.010@gmail.com>
2024-02-17 01:11:35 -0500

    Writes interface for a deque.

---
+++ for-pp2/deque.hpp
@@ -0,0 +1,29 @@
+#include "dll.hpp"
+
+#ifndef DEQUE_H
+#define DEQUE_H
+
+class Deque {
+    public:
+        Deque();
+
+        // We don't need a destructor, since Deque itself does not allocate.
+        // ~Deque();
+        
+        bool empty() const;
+        
+        void push_to_front(const char &c);
+        void push_to_back(const char &c);
+
+        char pop_front();
+        char pop_back();
+
+        char peek_front() const;
+        char peek_back() const;
+
+    private:
+        DLL dll;
+        int n;
+};
+
+#endif
